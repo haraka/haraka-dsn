dc93256ca5e96e010453a8ee2eb9eaf961c36930
Matt Simerson <matt@tnpi.net>
2024-04-04 15:30:37 -0700

    release 1.0.5
    - test: cleanups
    - dev: dep shedding
    - package.json: declare files, reduce module pack size
    - ci: add on.pull_request

--- test/index.js
+++ test/index.js
@@ -1,4 +1,3 @@
-'use strict';
 
 const assert = require('assert')
 
@@ -7,40 +6,35 @@ const constants = require('haraka-constants');
 constants.import(global);
 
 describe('haraka-dsn', function () {
-    beforeEach(function (done) {
+    beforeEach(function () {
         this.DSN = require('../index')
-        done()
     })
 
-
-    it('should load', function (done) {
+    it('should load', function () {
         assert.ok(this.DSN);
-        done()
     })
 
-    it('create, only code', function (done) {
+    it('create, only code', function () {
         assert.deepEqual({
             code: 200, msg: undefined, cls: 2, sub: 0, det: 0,
             default_msg: 'Other undefined status',
             reply: '2.0.0 Other undefined status'
         },
         this.DSN.create(200)
-        );
-        done()
+        )
     })
 
-    it('create, code w/msg', function (done) {
+    it('create, code w/msg', function () {
         assert.deepEqual({
             code: 200, msg: 'test msg', cls: 2, sub: 0, det: 0,
             default_msg: 'Other undefined status',
             reply: '2.0.0 test msg'
         },
         this.DSN.create(200, 'test msg')
-        );
-        done()
+        )
     })
 
-    it('create, code w/msg & subject', function (done) {
+    it('create, code w/msg & subject', function () {
         assert.deepEqual({
             code: 200, msg: 'test msg', cls: 2, sub: 7, det: 0,
             default_msg: 'Other or undefined security status',
@@ -48,20 +42,17 @@ describe('haraka-dsn', function () {
         },
         this.DSN.create(200, 'test msg', 7)
         );
-        done()
     })
 
-    it('net_unspecified returns expected code & message', function (done) {
+    it('net_unspecified returns expected code & message', function () {
         const r = this.DSN.net_unspecified()
         assert.equal(r.code, 450)
         assert.equal(r.default_msg, 'Other or undefined network or routing status')
-        done()
     })
 
-    it('net_unable_to_route returns expected code & message', function (done) {
+    it('net_unable_to_route returns expected code & message', function () {
         const r = this.DSN.net_unable_to_route()
         assert.equal(r.code, 550)
         assert.equal(r.default_msg, 'Unable to route')
-        done()
     })
 })
