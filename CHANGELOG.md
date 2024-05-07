1446f448ea41e564f4f52826ebae862904595a4f
Matt Simerson <matt@tnpi.net>
2024-05-07 16:36:36 -0700

    Release v1.1.0 (#23)
    - add/update DSNs from RFCs 3886, 4468, 4954
    - doc(README): improve formatting, add RFCs to Reference
    - doc(CONTRIBUTORS): added

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,16 @@
+# Changelog
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/).
 
 ### Unreleased
+
+### [1.1.0] - 2024-05-07
+
+- add/update DSNs from RFCs 3886, 4468, 4954
+- doc(README): improve formatting, add RFCs to Reference
+- doc(CONTRIBUTORS): added
+- dep: eslint-plugin-haraka -> @haraka/eslint-config
+- chore(ci): updated to latest shared GHA configs
 
 ### 1.0.5 - 2024-04-04
 
@@ -8,13 +19,11 @@
 - test: remove useless use of done
 - ci: add on.pull_request
 
-
 ### 1.0.4 - 2022-06-05
 
 - ci: update with shared GHA workflows
 - ci: add node 18 testing
 - ci: replace travis & appveyor with github actions
-
 
 ### 1.0.3 - 2020-01-17
 
@@ -25,14 +34,14 @@
 - Fix execution rights on run_tests #2
 - Replaced concatenated strings with template literals #1
 
-
 ### 1.0.2 - 2017-09-11
 
 - es6: use arrow functions
-
 
 ### 1.0.1 - 2017-09-04
 
 - initial release
 
-
+[1.0.4]: https://github.com/haraka/haraka-dsn/releases/tag/v1.0.4
+[1.0.5]: https://github.com/haraka/haraka-dsn/releases/tag/v1.0.5
+[1.1.0]: https://github.com/haraka/haraka-dsn/releases/tag/v1.1.0
--- test/index.js
+++ test/index.js
@@ -1,58 +1,72 @@
-
 const assert = require('assert')
 
-const constants = require('haraka-constants');
+const constants = require('haraka-constants')
 
-constants.import(global);
+constants.import(global)
 
 describe('haraka-dsn', function () {
-    beforeEach(function () {
-        this.DSN = require('../index')
-    })
+  beforeEach(function () {
+    this.DSN = require('../index')
+  })
 
-    it('should load', function () {
-        assert.ok(this.DSN);
-    })
+  it('should load', function () {
+    assert.ok(this.DSN)
+  })
 
-    it('create, only code', function () {
-        assert.deepEqual({
-            code: 200, msg: undefined, cls: 2, sub: 0, det: 0,
-            default_msg: 'Other undefined status',
-            reply: '2.0.0 Other undefined status'
-        },
-        this.DSN.create(200)
-        )
-    })
+  it('create, only code', function () {
+    assert.deepEqual(
+      {
+        code: 200,
+        msg: undefined,
+        cls: 2,
+        sub: 0,
+        det: 0,
+        default_msg: 'Other undefined status',
+        reply: '2.0.0 Other undefined status',
+      },
+      this.DSN.create(200),
+    )
+  })
 
-    it('create, code w/msg', function () {
-        assert.deepEqual({
-            code: 200, msg: 'test msg', cls: 2, sub: 0, det: 0,
-            default_msg: 'Other undefined status',
-            reply: '2.0.0 test msg'
-        },
-        this.DSN.create(200, 'test msg')
-        )
-    })
+  it('create, code w/msg', function () {
+    assert.deepEqual(
+      {
+        code: 200,
+        msg: 'test msg',
+        cls: 2,
+        sub: 0,
+        det: 0,
+        default_msg: 'Other undefined status',
+        reply: '2.0.0 test msg',
+      },
+      this.DSN.create(200, 'test msg'),
+    )
+  })
 
-    it('create, code w/msg & subject', function () {
-        assert.deepEqual({
-            code: 200, msg: 'test msg', cls: 2, sub: 7, det: 0,
-            default_msg: 'Other or undefined security status',
-            reply: '2.7.0 test msg'
-        },
-        this.DSN.create(200, 'test msg', 7)
-        );
-    })
+  it('create, code w/msg & subject', function () {
+    assert.deepEqual(
+      {
+        code: 200,
+        msg: 'test msg',
+        cls: 2,
+        sub: 7,
+        det: 0,
+        default_msg: 'Authentication Succeeded',
+        reply: '2.7.0 test msg',
+      },
+      this.DSN.create(200, 'test msg', 7),
+    )
+  })
 
-    it('net_unspecified returns expected code & message', function () {
-        const r = this.DSN.net_unspecified()
-        assert.equal(r.code, 450)
-        assert.equal(r.default_msg, 'Other or undefined network or routing status')
-    })
+  it('net_unspecified returns expected code & message', function () {
+    const r = this.DSN.net_unspecified()
+    assert.equal(r.code, 450)
+    assert.equal(r.default_msg, 'Other or undefined network or routing status')
+  })
 
-    it('net_unable_to_route returns expected code & message', function () {
-        const r = this.DSN.net_unable_to_route()
-        assert.equal(r.code, 550)
-        assert.equal(r.default_msg, 'Unable to route')
-    })
+  it('net_unable_to_route returns expected code & message', function () {
+    const r = this.DSN.net_unable_to_route()
+    assert.equal(r.code, 550)
+    assert.equal(r.default_msg, 'Unable to route')
+  })
 })
